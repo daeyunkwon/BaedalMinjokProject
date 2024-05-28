89ca7326bf2a30287cb7b50b251578306a28bade
Daeyun <sweet_ray@naver.com>
2024-05-28 14:01:37 +0900

    Initial Commit

---
+++ BaedalMinjok Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaedalMinjok Project
+//
+//  Created by 권대윤 on 5/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
